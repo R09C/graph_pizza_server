5ec8aa761ddad04778fd205efef239a88ee0fbed
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2023-07-05 19:20:36 +0300

    Auth generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
 	imports: [
@@ -11,6 +12,7 @@ import { UsersModule } from './users/users.module';
 		}),
 		PrismaModule,
 		UsersModule,
+		AuthModule,
 	],
 	controllers: [],
 	providers: [ PrismaService],
