6e2e54f5be4f599c8b0bf15a2105a44178c11da8
R09C <hodarev.ruslan@mail.ru>
2023-07-15 00:20:16 +0700

    create factory and fix repo

--- src/factory/factories/index.ts
+++ src/factory/factories/index.ts
@@ -1,4 +1,6 @@
 import { UserFactory } from './user.factory';
 import { ProductFactory } from './products.factory';
+import { CategoryFactory } from './category.factory';
+import { IngredientFactory } from './ingredient.factory';
 
-export const factories = [UserFactory, ProductFactory];
+export const factories = [UserFactory, ProductFactory, CategoryFactory, IngredientFactory];
